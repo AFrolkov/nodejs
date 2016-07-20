93732305158a162da6441b7115906a8408ea9268
unknown <art.frolkov@gmail.com>
2016-07-20 20:04:21 +0300

    app + logger + config

--- testExpress/app.js
+++ testExpress/app.js
@@ -8,12 +8,17 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var config = require('./config');
 var app = express();
+app.set('port', config.get('port'));
+//кривота конечно, чтобы передать переменную окружения
+module.exports = app;
+var log = require('./libs/log')(module);
+
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+
+/*app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -22,17 +27,41 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));*/
 
 // development only
-if ('development' == app.get('env')) {
+/*if ('development' == app.get('env')) {
   app.use(express.errorHandler());
-}
-
+}*/
+/*
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users', user.list);*/
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-  debugger;
+app.use(function(req, res, next) {
+	if (req.url == '/') {
+		res.send('HELLO');
+	} else {
+		next();
+	}
 });
+
+app.use(function(req, res, next) {
+	if (req.url == '/re') {
+		next(new Error('zzz'));
+	} else {
+		next();
+	}
+});
+
+app.use(function(err, req, res, next) {
+	var err1 = express.errorHandler({showStack: true});
+	err1(err, req, res, next);
+});
+
+
+
+http.createServer(app).listen(config.get('port'), function(){
+	log.info('Express server listening on port ' + config.get('port'));
+});
+
+module.exports = app;
---
+++ testExpress/libs/log.js
@@ -0,0 +1,19 @@
+var winston = require('winston');
+var env = require('../app').get('env');
+
+function getLogger(module) {
+
+	var path = module.filename.split('\\').slice(-2).join('\\');
+
+	return new winston.Logger({
+		transports: [
+			new winston.transports.Console({
+				colorize: true,
+				level: env == 'development' ? 'debug' : 'error' ,
+				label: path
+			})
+		]
+	});
+}
+
+module.exports = getLogger;
