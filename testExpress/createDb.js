736ce88342f70a1f07f824e9cac6743db6ff472a
unknown <art.frolkov@gmail.com>
2016-08-26 18:52:37 +0300

    mongoose part2

--- testExpress/createDb.js
+++ testExpress/createDb.js
@@ -24,9 +24,9 @@ MongoClient.connect('mongodb://127.0.0.1/test', function(err, db) {
 	});
 });*/
 
-var User = require('./models/user').User;
+/*var User = require('./models/user').User;
 
-/*var user = new User({
+var user = new User({
 	username: 'Frolkov',
 	password: 'secret'
 })
@@ -37,8 +37,38 @@ user.save(function(err, user, affected) {
 	}
 
 	console.log('done');
-});*/
+});
 
 User.findOne({username: 'Frolkov'}, function(err, data) {
 	console.log(data);
+});*/
+
+//connect
+//drop
+//new users -> save
+//close
+
+var mongoose = require('./libs/mongoose');
+var async = require('async');
+mongoose.set('debug', true);
+
+mongoose.connection.on('open', function() {
+	mongoose.connection.db.dropDatabase(function(err) {
+		if (err) throw err;
+		console.log('droped');
+
+		require('./models/user');
+
+		async.parallel([
+			function(callback) {
+				var artem = new mongoose.models.User({username: "Artem", password: "123"});
+				artem.save(function(err) {
+					callback(err, artem);
+				});
+			}
+			], function(err, results) {
+			console.log(arguments)
+		});
+
+	});
 });
